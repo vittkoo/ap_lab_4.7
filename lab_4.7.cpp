088209d3d05c66c6eff2cc9d1da81b45e55cf9fa
vittkoo <dmytrovych.vv@gmail.com>
2024-09-28 05:03:11 +0300

    commit_1

---
+++ lab_4.7.cpp
@@ -0,0 +1,50 @@
+#include <iostream> 
+#include <iomanip> 
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+    double xp, xk, x, dx, eps, a = 0, R = 0, S = 0, Pi = 4 * atan(1.0);
+    int n=0;
+ 
+    cout << "xp = "; cin >> xp; 
+    cout << "xk = "; cin >> xk; 
+    cout << "dx = "; cin >> dx; 
+    cout << "eps = "; cin >> eps; 
+ 
+    cout << fixed; 
+        cout << "-------------------------------------------------" << endl; 
+        cout << "|" << setw(5)  << "x"      << "     |" 
+                    << setw(10) << "arccot(x)" << "   |"  
+                    << setw(7) << "S"      << "      |"  
+                    << setw(5)  << "n"      << "   |"  
+                    << endl; 
+        cout << "-------------------------------------------------" << endl; 
+ 
+    x = xp; 
+    while (x <= xk) 
+    { 
+        n = 0;
+        a = x;
+        S = a; 
+        do{ 
+            n++; 
+            R = (pow(-1, n + 1) * pow(x, 2 * n + 1)) / (2 * n + 1);
+            a *= R; 
+            S += a; 
+        }while (abs(a)>=eps); 
+
+        S = Pi / 2 + S;
+ 
+        cout << "|" << setw(7) << setprecision(2) << x << "   |"
+            << setw(10) << setprecision(5) << atan(x) << "   |"
+            << setw(10) << setprecision(5) << S << "   |"
+            << setw(5) << n << "   |"
+            << endl;
+        x += dx; 
+    } 
+        cout << "-------------------------------------------------" << endl; 
+ 
+    return 0; 
+}
